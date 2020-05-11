c7180db67b408e75b39c18f2188df538fb3f06ef
Sven Wappler <info@wappler.systems>
2020-05-12 00:22:22 +0200

    Changes

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Form Extended',
+    'description' => '',
+    'category' => 'misc',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7-10.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
