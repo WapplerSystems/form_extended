d328d47d46b357d304038ee38a7e0c18382baf03
Sven Wappler <info@wappler.systems>
2020-09-11 17:42:04 +0200

    updated dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,6 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '8.7-10.4.99',
+            'form' => '8.7-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
