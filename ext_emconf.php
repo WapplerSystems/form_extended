1700ffe49c6c9a8261007ef7dac0a5d67f033eb4
Sven Wappler <info@wappler.systems>
2020-12-09 00:22:06 +0100

    branch for TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
-    'version' => '8.0.0',
+    'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7-8.7.99',
-            'form' => '8.7-8.7.99'
+            'typo3' => '10.4.0-10.4.99',
+            'form' => '10.4.0-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
