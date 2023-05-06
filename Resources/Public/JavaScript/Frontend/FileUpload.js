2554a0a2d2150dfed08209531bf7355883e401f3
Sven Wappler <info@wappler.systems>
2023-05-06 03:04:08 +0200

    jquery to vanilla

--- Resources/Public/JavaScript/Frontend/FileUpload.js
+++ Resources/Public/JavaScript/Frontend/FileUpload.js
@@ -1,40 +1,58 @@
 
-
-$("input[multiple]").each(function () {
-  let element = this;
-  const dt = new DataTransfer();
-
-  $(element).on('change', function (e) {
-    for (let i = 0; i < element.files.length; i++) {
-      let fileBloc = $('<span/>', {class: 'multiupload-file-block'}),
-        fileName = $('<span/>', {class: 'name', text: this.files.item(i).name});
-      fileBloc.append('<span class="multiupload-file-delete"><span>+</span></span>')
-        .append(fileName);
-      $(element).nextAll('.multiupload-files').append(fileBloc);
+function multiuploadNextAll(element, selector) {
+  let siblings = [];
+  while (element = element.nextElementSibling) {
+    if (element.matches(selector)) {
+      siblings.push(element);
     }
-    for (let file of element.files) {
-      dt.items.add(file);
-    }
-    element.files = dt.files;
+  }
+  return siblings;
+}
 
-  });
-
-  $('.multiupload-file-delete',$(element).nextAll('.multiupload-files')).click(function () {
-    let resource = $(this).parent().attr('data-target');
+function addListenerToMultiUploadDeleteButton(deleteBtn,dt,inputField) {
+  deleteBtn.addEventListener('click', function () {
+    let resource = this.parentNode.getAttribute('data-target');
     if (resource) {
-      $(resource).remove();
+      document.querySelector(resource).remove();
     }
-    let name = $(this).next('span.name').text();
-    $(this).parent().remove();
+    let name = this.nextElementSibling.innerText;
+    this.parentNode.remove();
     for (let i = 0; i < dt.items.length; i++) {
       if (name === dt.items[i].getAsFile().name) {
         dt.items.remove(i);
       }
     }
+    inputField.files = dt.files;
+  });
+}
+
+document.querySelectorAll('input[multiple]').forEach(function (element) {
+  const dt = new DataTransfer();
+
+  element.addEventListener('change', function (e) {
+    for (let i = 0; i < element.files.length; i++) {
+      let fileBloc = document.createElement('span');
+      fileBloc.classList.add('multiupload-file-block');
+      let fileName = document.createElement('span');
+      fileName.classList.add('name');
+      fileName.innerText = this.files.item(i).name;
+      let n = document.createElement('span');
+      n.classList.add('multiupload-file-delete');
+      let m = document.createElement('span');
+      m.innerHTML = '+';
+      n.appendChild(m);
+      addListenerToMultiUploadDeleteButton(n,dt,element);
+      fileBloc.appendChild(n);
+      fileBloc.appendChild(fileName);
+      multiuploadNextAll(element, '.multiupload-files')[0].appendChild(fileBloc);
+    }
+    for (let file of element.files) {
+      dt.items.add(file);
+    }
     element.files = dt.files;
   });
 
+  multiuploadNextAll(element, '.multiupload-files')[0].querySelectorAll('.multiupload-file-delete').forEach(function (deleteBtn) {
+    addListenerToMultiUploadDeleteButton(deleteBtn,dt,element);
+  });
 });
-
-
-
