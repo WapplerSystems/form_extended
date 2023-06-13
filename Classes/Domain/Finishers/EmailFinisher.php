acdcf2ea8f347364cb789ae7afa924aa5c25b0ae
Sven Wappler <info@wappler.systems>
2023-06-13 13:35:44 +0200

    Multi upload files can be now attached to email

---
+++ Classes/Domain/Finishers/EmailFinisher.php
@@ -0,0 +1,191 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace WapplerSystems\FormExtended\Domain\Finishers;
+
+use Symfony\Component\Mime\Address;
+use TYPO3\CMS\Core\Mail\FluidEmail;
+use TYPO3\CMS\Core\Mail\Mailer;
+use TYPO3\CMS\Core\Mail\MailMessage;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Form\Domain\Finishers\AbstractFinisher;
+use TYPO3\CMS\Form\Domain\Finishers\Exception\FinisherException;
+use TYPO3\CMS\Form\Domain\Model\FormElements\FileUpload;
+use TYPO3\CMS\Form\Service\TranslationService;
+
+/**
+ * This finisher sends an email to one recipient
+ *
+ * Options:
+ *
+ * - templatePathAndFilename (mandatory for Mail): Template path and filename for the mail body
+ * - templateName (mandatory for FluidEmail): Template name for the mail body
+ * - templateRootPaths: root paths for the templates
+ * - layoutRootPaths: root paths for the layouts
+ * - partialRootPaths: root paths for the partials
+ * - variables: associative array of variables which are available inside the Fluid template
+ *
+ * The following options control the mail sending. In all of them, placeholders in the form
+ * of {...} are replaced with the corresponding form value; i.e. {email} as senderAddress
+ * makes the recipient address configurable.
+ *
+ * - subject (mandatory): Subject of the email
+ * - recipients (mandatory): Email addresses and human-readable names of the recipients
+ * - senderAddress (mandatory): Email address of the sender
+ * - senderName: Human-readable name of the sender
+ * - replyToRecipients: Email addresses and human-readable names of the reply-to recipients
+ * - carbonCopyRecipients: Email addresses and human-readable names of the copy recipients
+ * - blindCarbonCopyRecipients: Email addresses and human-readable names of the blind copy recipients
+ *
+ * Scope: frontend
+ */
+class EmailFinisher extends \TYPO3\CMS\Form\Domain\Finishers\EmailFinisher
+{
+
+    /**
+     * Executes this finisher
+     * @see AbstractFinisher::execute()
+     *
+     * @throws FinisherException
+     */
+    protected function executeInternal()
+    {
+        $languageBackup = null;
+        // Flexform overrides write strings instead of integers so
+        // we need to cast the string '0' to false.
+        if (
+            isset($this->options['addHtmlPart'])
+            && $this->options['addHtmlPart'] === '0'
+        ) {
+            $this->options['addHtmlPart'] = false;
+        }
+
+        $subject = (string)$this->parseOption('subject');
+        $recipients = $this->getRecipients('recipients');
+        $senderAddress = $this->parseOption('senderAddress');
+        $senderAddress = is_string($senderAddress) ? $senderAddress : '';
+        $senderName = $this->parseOption('senderName');
+        $senderName = is_string($senderName) ? $senderName : '';
+        $replyToRecipients = $this->getRecipients('replyToRecipients');
+        $carbonCopyRecipients = $this->getRecipients('carbonCopyRecipients');
+        $blindCarbonCopyRecipients = $this->getRecipients('blindCarbonCopyRecipients');
+        $addHtmlPart = $this->parseOption('addHtmlPart') ? true : false;
+        $attachUploads = $this->parseOption('attachUploads');
+        $useFluidEmail = $this->parseOption('useFluidEmail');
+        $title = (string)$this->parseOption('title') ?: $subject;
+
+        if ($subject === '') {
+            throw new FinisherException('The option "subject" must be set for the EmailFinisher.', 1327060320);
+        }
+        if (empty($recipients)) {
+            throw new FinisherException('The option "recipients" must be set for the EmailFinisher.', 1327060200);
+        }
+        if (empty($senderAddress)) {
+            throw new FinisherException('The option "senderAddress" must be set for the EmailFinisher.', 1327060210);
+        }
+
+        $formRuntime = $this->finisherContext->getFormRuntime();
+
+        $translationService = GeneralUtility::makeInstance(TranslationService::class);
+        if (is_string($this->options['translation']['language'] ?? null) && $this->options['translation']['language'] !== '') {
+            $languageBackup = $translationService->getLanguage();
+            $translationService->setLanguage($this->options['translation']['language']);
+        }
+
+        $mail = $useFluidEmail
+            ? $this
+                ->initializeFluidEmail($formRuntime)
+                ->format($addHtmlPart ? FluidEmail::FORMAT_BOTH : FluidEmail::FORMAT_PLAIN)
+                ->assign('title', $title)
+            : GeneralUtility::makeInstance(MailMessage::class);
+
+        $mail
+            ->from(new Address($senderAddress, $senderName))
+            ->to(...$recipients)
+            ->subject($subject);
+
+        if (!empty($replyToRecipients)) {
+            $mail->replyTo(...$replyToRecipients);
+        }
+
+        if (!empty($carbonCopyRecipients)) {
+            $mail->cc(...$carbonCopyRecipients);
+        }
+
+        if (!empty($blindCarbonCopyRecipients)) {
+            $mail->bcc(...$blindCarbonCopyRecipients);
+        }
+
+        if (!$useFluidEmail) {
+            $parts = [
+                [
+                    'format' => 'Plaintext',
+                    'contentType' => 'text/plain',
+                ],
+            ];
+
+            if ($addHtmlPart) {
+                $parts[] = [
+                    'format' => 'Html',
+                    'contentType' => 'text/html',
+                ];
+            }
+
+            foreach ($parts as $i => $part) {
+                $standaloneView = $this->initializeStandaloneView($formRuntime, $part['format']);
+                $message = $standaloneView->render();
+
+                if ($part['contentType'] === 'text/plain') {
+                    $mail->text($message);
+                } else {
+                    $mail->html($message);
+                }
+            }
+        }
+
+        if (!empty($languageBackup)) {
+            $translationService->setLanguage($languageBackup);
+        }
+
+        if ($attachUploads) {
+            foreach ($formRuntime->getFormDefinition()->getRenderablesRecursively() as $element) {
+                if (!$element instanceof FileUpload) {
+                    continue;
+                }
+                $file = $formRuntime[$element->getIdentifier()];
+                if ($file) {
+                    if (is_array($file)) {
+                        foreach ($file as $singleFile) {
+                            if ($singleFile instanceof FileReference) {
+                                $singleFile = $singleFile->getOriginalResource();
+                            }
+                            $mail->attach($singleFile->getContents(), $singleFile->getName(), $singleFile->getMimeType());
+                        }
+                        continue;
+                    }
+                    if ($file instanceof FileReference) {
+                        $file = $file->getOriginalResource();
+                    }
+                    $mail->attach($file->getContents(), $file->getName(), $file->getMimeType());
+                }
+            }
+        }
+
+        $useFluidEmail ? GeneralUtility::makeInstance(Mailer::class)->send($mail) : $mail->send();
+    }
+}
