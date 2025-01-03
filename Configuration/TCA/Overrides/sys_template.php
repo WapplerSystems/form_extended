68e9c223e9b6aac03fe906a55f5182b8fdb4c18a
Sven Wappler <wappler@wappler.systems>
2025-01-03 17:03:57 +0100

    updates

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,10 +6,5 @@
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-ExtensionManagementUtility::registerPageTSConfigFile(
-'form_extended',
-'Configuration/TsConfig/NewContentElement.tsconfig',
-'Form Double Opt-In'
-);
 
 ExtensionManagementUtility::addStaticFile('form_extended', 'Configuration/TypoScript/', 'Form Extended');
