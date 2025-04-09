58a24bb71f869bd453f546bf8d39bb9aad2c12ad
Sven Wappler <wappler@wappler.systems>
2025-04-09 15:04:12 +0200

    changes

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,10 +1,5 @@
 <?php
 
-/***************
-* Add content element PageTSConfig
-*/
-
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 
 ExtensionManagementUtility::addStaticFile('form_extended', 'Configuration/TypoScript/', 'Form Extended');
