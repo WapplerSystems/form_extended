6f145f1701a2be890024e4bec0fb8d6cedabb00b
Sven Wappler <wappler@wappler.systems>
2025-04-13 00:02:46 +0200

    version 13.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
-$EM_CONF['form_extended'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Form Extended',
-    'description' => 'Multi upload field, double opt in, tag select field and other',
+    'description' => 'Multi upload field, sender addresses in site config, new field types and other',
     'category' => 'misc',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
-    'version' => '13.2.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '13.0.0-13.4.99',
