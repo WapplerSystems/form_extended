1e6760e355e2bbc34d36101c20bad59f1aa13667
Sven Wappler <wappler@wappler.systems>
2025-11-30 00:24:51 +0100

    Update TYPO3 and form version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '13.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.4.99',
-            'form' => '13.0.0-13.4.99'
+            'typo3' => '14.0.0-14.4.99',
+            'form' => '14.0.0-14.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
